ed15fdc111c593aec2b36abcb19319d94407c556
Salman Mohammadi <salman.mohammadi@outlook.com>
2024-09-08 18:22:12 +0100

    updating conf

--- download_weights.sh
+++ download_weights.sh
@@ -1,5 +1,9 @@
+pip3 install torch torchao --index-url https://download.pytorch.org/whl/cu124 --force-reinstall
+pip install -e .["dev"]
 tune download Skywork/Skywork-Reward-Llama-3.1-8B --output-dir /tmp/reward/ --ignore-patterns " "
 tune download meta-llama/Meta-Llama-3.1-8B-Instruct --output-dir /tmp/policy_model --ignore-patterns "original/consolidated.00.pth"
 mkdir /tmp/ppo
 mkdir /tmp/ppo/policy/
 mkdir /tmp/ppo/value/
+
+echo "tune run lora_ppo_finetune_single_device --config recipes/configs/llama3_1/8b_ppo_qlora.yaml"
